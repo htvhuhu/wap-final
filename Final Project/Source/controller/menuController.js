76dc91f38e0f16f138ebb23918f7b5a140b425e9
lam9800585 <lam9800585@gmail.com>
2023-08-15 07:48:40 -0500

    added review

--- Final Project/Source/controller/menuController.js
+++ Final Project/Source/controller/menuController.js
@@ -4,8 +4,7 @@ const connection = require('../dbConnector');
 const dbHelper = require('../helper/dbHelper'); 
 
 
-module.exports.showMenu = (req, res, next) => {    
-    (async () => {
+module.exports.showMenu = async (req, res, next) => {       
         try {            
             let menu = await dbHelper.queryDatabase('SELECT * FROM category');
             let dishes = await dbHelper.queryDatabase('SELECT * FROM dish');          
@@ -14,29 +13,56 @@ module.exports.showMenu = (req, res, next) => {
             console.error(error);
         } finally {
            
-        }
-    })();  
-    
+        } 
+   
 }
 
-module.exports.loadFoodDetail = (req, res, next) => {    
-    res.render('foodDetail');     
+module.exports.loadDishDetail = async  (req, res, next) => {    
+    var id = req.params.id;
+       
+        try {            
+            let query = 'SELECT * FROM dish WHERE dishId=' + id;            
+
+            let arrDish = await dbHelper.queryDatabase(query); 
+            let dish = arrDish[0];
+            query = 'SELECT * FROM review WHERE dishId=' + id;  
+            let reviews = await dbHelper.queryDatabase(query);           
+                
+            res.render("dish", {objDish:dish, ingredients:dish.ingredients.split(','), objReview:reviews }) ;            
+        } catch (error) {
+            console.error(error);
+        } finally {
+           
+        }      
+       
 }
 
-module.exports.loadFoodByCategoryId = (req, res, next) => {    
-    (async () => {
+module.exports.loadDishByCategoryId = async (req, res, next) => {     
         try {            
             let query = 'SELECT * FROM dish ';
             if (req.params.id != '0')
                 query = query + ' WHERE catId=' + req.params.id;
 
             let dishes = await dbHelper.queryDatabase(query);          
-            res.render("menuPartial", {objDish:dishes})          
+            res.render("partials/_dishList", {objDish:dishes})          
         } catch (error) {
             console.error(error);
         } finally {
            
-        }
-    })();  
+        }    
 }
 
+module.exports.addComment = async (req, res, next) => {    
+        try {       
+            let query = `INSERT INTO review(dishId, revName, revDetail) VALUES('${req.body.dishId}','${req.body.name}','${req.body.comment}')`
+            dbHelper.queryDatabase(query);    
+
+            query = 'SELECT * FROM review WHERE dishId=' + req.body.dishId;  
+            let reviews = await dbHelper.queryDatabase(query);
+            res.render("partials/_menuComment", {objReview:reviews})              
+        } catch (error) {
+            console.error(error);
+        } finally {
+           
+        }    
+}
--- Final Project/Source/routes/menuRouter.js
+++ Final Project/Source/routes/menuRouter.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const controller = require('../controller/menuController');
 
 router.get('/', controller.showMenu)
-router.get('/foodDetail', controller.loadFoodDetail)
-router.get('/:id', controller.loadFoodByCategoryId)
+router.get('/dish/:id', controller.loadDishDetail)
+router.get('/:id', controller.loadDishByCategoryId)
+router.post('/comment', controller.addComment)
 
 module.exports = router;
